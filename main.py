044e119a6b15694c63c54d8ce062dc5da4766c11
TylBohdan <u17102003@gmail.com>
2023-02-18 21:34:06 +0200

    Set clock

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame
 from consts import ICON_WIDTH, ICON_HEIGHT
 from images import icon, background, crosshair, bird_images
 
+clock = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode((ICON_WIDTH, ICON_HEIGHT))
 pygame.display.set_caption('Duck Hunt')
+
+running = True
+while running:
+    pygame.display.update()
+
+    clock.tick(9)
