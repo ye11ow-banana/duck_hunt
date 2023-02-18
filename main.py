4f98f4cadde510ef761caf88abc00e23687872cf
Mykhailo <mikhail.yakubets@gmail.com>
2023-02-18 20:47:10 +0200

    Add images loading

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 
+from images import icon, background, crosshair, bird_images
+
+
 pygame.init()
 screen = pygame.display.set_mode((920, 521))
 pygame.display.set_caption('Duck Hunt')
