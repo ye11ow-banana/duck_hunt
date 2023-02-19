fb096a48d8778b01423686e35c36d110058ec7c8
TylBohdan <u17102003@gmail.com>
2023-02-19 19:45:31 +0200

    Change _get_bird_images method for working with BirdImages data structure

--- entities.py
+++ entities.py
@@ -1,5 +1,7 @@
 import pygame
 
+from data_structures import BirdImages, NonSpawnedBorder, BirdPosition
+
 
 class Bird:
     def __init__(self, level):
@@ -10,18 +12,20 @@ class Bird:
         self.non_spawned_border = None
         self.position = None
 
-    def _get_bird_images(self, bird_types, directions):
-        bird_images = {}
-        for bird_type in bird_types:
-            bird_directions = {}
-            for direction in directions:
-                images = []
-                for _ in range(1, 4):
-                    image_path = f'images/birds/{bird_type}/{direction}_{_}.png'
-                    try:
-                        images.append(pygame.image.load(image_path))
-                    except FileNotFoundError:
-                        pass
-                bird_directions[direction] = images
-            bird_images[bird_type] = bird_directions
-        return bird_images
+    def _get_bird_images(self) -> BirdImages:
+        bird_directions = {}
+        for direction in 'top', 'top_right', 'right', 'bottom':
+            images = []
+            for _ in range(1, 4):
+                image_path = f'images/birds/{self.level}/{direction}_{_}.png'
+                try:
+                    images.append(pygame.image.load(image_path))
+                except FileNotFoundError:
+                    pass
+            bird_directions[direction] = images
+        return BirdImages(
+            top=bird_directions['top'],
+            top_right=bird_directions['top_right'],
+            right=bird_directions['right'],
+            bottom=bird_directions['bottom']
+        )
