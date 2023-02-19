0fa3088819d7a619a0271af2de7293e4742d12d1
TylBohdan <u17102003@gmail.com>
2023-02-19 19:41:35 +0200

    Move _get_bird_images method to Bird class

--- entities.py
+++ entities.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 class Bird:
     def __init__(self, level):
         self.is_killed = False
@@ -6,3 +9,19 @@ class Bird:
         self.images = None
         self.non_spawned_border = None
         self.position = None
+
+    def _get_bird_images(self, bird_types, directions):
+        bird_images = {}
+        for bird_type in bird_types:
+            bird_directions = {}
+            for direction in directions:
+                images = []
+                for _ in range(1, 4):
+                    image_path = f'images/birds/{bird_type}/{direction}_{_}.png'
+                    try:
+                        images.append(pygame.image.load(image_path))
+                    except FileNotFoundError:
+                        pass
+                bird_directions[direction] = images
+            bird_images[bird_type] = bird_directions
+        return bird_images
