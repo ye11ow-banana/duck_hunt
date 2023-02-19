8c2e4d3e8d6d4a59d2e4e7a9230dadb6a5f0192a
TylBohdan <u17102003@gmail.com>
2023-02-19 19:37:55 +0200

    Add Bird entity class

---
+++ entities.py
@@ -0,0 +1,8 @@
+class Bird:
+    def __init__(self, level):
+        self.is_killed = False
+        self.level = level
+        self.speed = None
+        self.images = None
+        self.non_spawned_border = None
+        self.position = None
