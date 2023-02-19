66dc78539a36f80a7941db1a967d0e377117d0de
Mykhailo <mikhail.yakubets@gmail.com>
2023-02-19 19:55:51 +0200

    Delete utils.py

--- images.py
+++ images.py
@@ -1,10 +1,6 @@
 import pygame
 
-from utils import get_bird_images
-from consts import BIRD_TYPES, DIRECTIONS
 
 icon = pygame.image.load('images/icon.png')
 background = pygame.image.load('images/background.webp')
 crosshair = pygame.image.load('images/crosshair.jpg')
-
-bird_images = get_bird_images(BIRD_TYPES, DIRECTIONS)
